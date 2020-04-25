8e07e78cf952a09f145365e82cc40028d2ed5a3a
cerisilv123 <60038915+cerisilv123@users.noreply.github.com>
2020-04-25 17:51:30 +0100

    Initial Commit

---
+++ project15-challenge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project15-challenge
+//
+//  Created by Ceri Silvester on 25/04/2020.
+//  Copyright © 2020 Ceri Silvester. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
